89c878f8f50a7a14c994a9e10c2c3569858516e6
shalu09 <2k22.ece.2212919@gmail.com>
2026-09-06 21:37:28 +0530

    display git repository

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/repository.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (string[] args) {
+        // Display the repository cloning status.
+        System.out.println("Repository: Cloned Succeccfully");
+
+       // Display whether the project is ready.
+       System.out.println("Project Status: Ready for Development");
+    }
+}
